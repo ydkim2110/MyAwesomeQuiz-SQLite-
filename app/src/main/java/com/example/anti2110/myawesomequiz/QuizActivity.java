b7a7f72e286d433789a11ddc44918084b892e20d
ydkim2110 <ydkim2110@gmail.com>
2018-08-26 23:05:39 +0900

    displaying questions and logging answers

--- app/src/main/java/com/example/anti2110/myawesomequiz/QuizActivity.java
+++ app/src/main/java/com/example/anti2110/myawesomequiz/QuizActivity.java
@@ -1,12 +1,17 @@
 package com.example.anti2110.myawesomequiz;
 
+import android.content.res.ColorStateList;
+import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import java.util.Collections;
 import java.util.List;
 
 public class QuizActivity extends AppCompatActivity {
@@ -21,7 +26,15 @@ public class QuizActivity extends AppCompatActivity {
     private RadioButton rb3;
     private Button btn_confirm_next;
 
+    private ColorStateList textColorDefaultRb;
+
     private List<Question> questionList;
+    private int quetionCounter;
+    private int questionCountTotal;
+    private Question currentQuestion;
+
+    private int score;
+    private boolean answered;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -38,9 +51,99 @@ public class QuizActivity extends AppCompatActivity {
         rb3 = findViewById(R.id.radio_btn3);
         btn_confirm_next = findViewById(R.id.btn_confirm_next);
 
+        textColorDefaultRb = rb1.getTextColors();
+
         QuizDbHelper dbHelper = new QuizDbHelper(this);
         questionList = dbHelper.getAllQuestions();
+        questionCountTotal = questionList.size();
+        Collections.shuffle(questionList);
 
+        showNextQuestion();
+
+        btn_confirm_next.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(!answered) {
+                    if(rb1.isChecked() || rb2.isChecked() || rb3.isChecked()) {
+                        checkAnswer();
+                    } else {
+                        Toast.makeText(QuizActivity.this, "Please select and answer", Toast.LENGTH_SHORT).show();
+                    }
+                } else {
+                    showNextQuestion();
+                }
+            }
+        });
+    }
+
+    private void checkAnswer() {
+        answered = true;
+
+        RadioButton rbSelected = findViewById(radioGroup.getCheckedRadioButtonId());
+        int answerNr = radioGroup.indexOfChild(rbSelected) + 1;
+
+        if(answerNr == currentQuestion.getAnswerNr()) {
+            score++;
+            tv_score.setText("Score: "+score);
+        }
+
+        showSolution();
+    }
+
+    private void showSolution() {
+        rb1.setTextColor(Color.RED);
+        rb2.setTextColor(Color.RED);
+        rb3.setTextColor(Color.RED);
+
+        switch (currentQuestion.getAnswerNr()) {
+            case 1:
+                rb1.setTextColor(Color.GREEN);
+                tv_question.setText("Answer 1 is correct");
+                break;
+            case 2:
+                rb2.setTextColor(Color.GREEN);
+                tv_question.setText("Answer 2 is correct");
+                break;
+            case 3:
+                rb3.setTextColor(Color.GREEN);
+                tv_question.setText("Answer 3 is correct");
+                break;
+        }
+
+        if(quetionCounter < questionCountTotal) {
+            btn_confirm_next.setText("Next");
+        } else {
+            btn_confirm_next.setText("Finish");
+        }
+    }
+
+    private void showNextQuestion() {
+        rb1.setTextColor(textColorDefaultRb);
+        rb2.setTextColor(textColorDefaultRb);
+        rb3.setTextColor(textColorDefaultRb);
+        radioGroup.clearCheck();
+
+        if(quetionCounter < questionCountTotal) {
+            currentQuestion = questionList.get(quetionCounter);
+
+            tv_question.setText(currentQuestion.getQuestion());
+            rb1.setText(currentQuestion.getOption1());
+            rb2.setText(currentQuestion.getOption2());
+            rb3.setText(currentQuestion.getOption3());
+
+            quetionCounter++;
+            tv_question_count.setText("Question: "+quetionCounter+"/"+questionCountTotal);
+            answered = false;
+            btn_confirm_next.setText("Confirm");
+
+        } else {
+            finishQuiz();
+        }
 
     }
+
+    private void finishQuiz() {
+        finish();
+    }
+
 }

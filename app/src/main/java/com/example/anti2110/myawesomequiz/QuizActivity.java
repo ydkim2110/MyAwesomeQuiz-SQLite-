5714a5286181c18ba609907e249fa53b53499bc2
ydkim2110 <ydkim2110@gmail.com>
2018-08-26 22:47:33 +0900

    rawquery database with cursor

--- app/src/main/java/com/example/anti2110/myawesomequiz/QuizActivity.java
+++ app/src/main/java/com/example/anti2110/myawesomequiz/QuizActivity.java
@@ -2,12 +2,45 @@ package com.example.anti2110.myawesomequiz;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.TextView;
+
+import java.util.List;
 
 public class QuizActivity extends AppCompatActivity {
+
+    private TextView tv_question;
+    private TextView tv_score;
+    private TextView tv_question_count;
+    private TextView tv_count_down;
+    private RadioGroup radioGroup;
+    private RadioButton rb1;
+    private RadioButton rb2;
+    private RadioButton rb3;
+    private Button btn_confirm_next;
+
+    private List<Question> questionList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_quiz);
+
+        tv_question = findViewById(R.id.tv_question);
+        tv_score = findViewById(R.id.tv_score);
+        tv_question_count = findViewById(R.id.tv_question_count);
+        tv_count_down = findViewById(R.id.tv_countdown);
+        radioGroup = findViewById(R.id.radio_group);
+        rb1 = findViewById(R.id.radio_btn1);
+        rb2 = findViewById(R.id.radio_btn2);
+        rb3 = findViewById(R.id.radio_btn3);
+        btn_confirm_next = findViewById(R.id.btn_confirm_next);
+
+        QuizDbHelper dbHelper = new QuizDbHelper(this);
+        questionList = dbHelper.getAllQuestions();
+
+
     }
 }
--- app/src/main/java/com/example/anti2110/myawesomequiz/QuizDbHelper.java
+++ app/src/main/java/com/example/anti2110/myawesomequiz/QuizDbHelper.java
@@ -2,9 +2,13 @@ package com.example.anti2110.myawesomequiz;
 
 import android.content.ContentValues;
 import android.content.Context;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 import com.example.anti2110.myawesomequiz.QuizContract.*;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class QuizDbHelper extends SQLiteOpenHelper {
 
@@ -62,4 +66,26 @@ public class QuizDbHelper extends SQLiteOpenHelper {
         cv.put(QuestionTable.COLUMN_ANSWER_NR, question.getAnswerNr());
         db.insert(QuestionTable.TABLE_NAME, null, cv);
     }
+
+    public List<Question> getAllQuestions() {
+        List<Question> questionList = new ArrayList<>();
+        db = getReadableDatabase();
+        Cursor c = db.rawQuery("SELECT * FROM "+QuestionTable.TABLE_NAME, null);
+
+        if(c.moveToFirst()) {
+            do {
+                Question question = new Question();
+                question.setQuestion(c.getString(c.getColumnIndex(QuestionTable.COLUMN_QUESTION)));
+                question.setOption1(c.getString(c.getColumnIndex(QuestionTable.COLUMN_OPTION1)));
+                question.setOption2(c.getString(c.getColumnIndex(QuestionTable.COLUMN_OPTION2)));
+                question.setOption3(c.getString(c.getColumnIndex(QuestionTable.COLUMN_OPTION3)));
+                question.setAnswerNr(c.getInt(c.getColumnIndex(QuestionTable.COLUMN_ANSWER_NR)));
+                questionList.add(question);
+            } while (c.moveToNext());
+        }
+
+        c.close();
+        return  questionList;
+    }
+
 }
